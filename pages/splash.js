87b2c4346a191fdadc05de961f38d3192bd81182
Hidro Sinú <hidrosinu.bot@gmail.com>
2026-07-01 00:30:02 -0500

    Update splash.js

--- pages/splash.js
+++ pages/splash.js
@@ -1,3 +1,9 @@
 setTimeout(() => {
-    window.location.href = "consulta-territorial.html";
+    const splash = document.querySelector('.splash-screen');
+    splash.classList.add('fade-out');
+
+    // Espera a que termine la animación de fundido (0.6s) antes de redirigir
+    setTimeout(() => {
+        window.location.href = "consulta-territorial.html";
+    }, 600);
 }, 3500);
